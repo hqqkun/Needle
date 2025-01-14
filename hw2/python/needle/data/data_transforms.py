fcca277350ad4ac058c5710573d8355af5432bc2
hqqkun <hqqkun1997@163.com>
2025-01-14 23:25:56 +0800

    code style

--- hw2/python/needle/data/data_transforms.py
+++ hw2/python/needle/data/data_transforms.py
@@ -1,12 +1,13 @@
 import numpy as np
 
+
 class Transform:
     def __call__(self, x):
         raise NotImplementedError
 
 
 class RandomFlipHorizontal(Transform):
-    def __init__(self, p = 0.5):
+    def __init__(self, p=0.5):
         self.p = p
 
     def __call__(self, img):
@@ -30,19 +31,25 @@ class RandomCrop(Transform):
     def __init__(self, padding=3):
         self.padding = padding
 
-    def __call__(self, img : np.ndarray):
-        """ Zero pad and then randomly crop an image.
+    def __call__(self, img: np.ndarray):
+        """Zero pad and then randomly crop an image.
         Args:
              img: H x W x C NDArray of an image
-        Return 
+        Return
             H x W x C NDArray of cliped image
         Note: generate the image shifted by shift_x, shift_y specified below
         """
-        shift_x, shift_y = np.random.randint(low=-self.padding, high=self.padding+1, size=2)
+        shift_x, shift_y = np.random.randint(
+            low=-self.padding, high=self.padding + 1, size=2
+        )
         ### BEGIN YOUR SOLUTION
         h, w, c = img.shape
-        padded_img = np.zeros((h + 2 * self.padding, w + 2 * self.padding, c), dtype=img.dtype)
-        padded_img[self.padding : self.padding + h, self.padding : self.padding + w, :] = img
+        padded_img = np.zeros(
+            (h + 2 * self.padding, w + 2 * self.padding, c), dtype=img.dtype
+        )
+        padded_img[
+            self.padding : self.padding + h, self.padding : self.padding + w, :
+        ] = img
         start_x = self.padding + shift_x
         start_y = self.padding + shift_y
         return padded_img[start_x : start_x + h, start_y : start_y + w, :]
